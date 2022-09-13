8f9e21b9639d623c79a37e1301d7bc01c8ed4df0
Martín Alexis Samán Arata <martinsam1610@gmail.com>
2022-09-13 06:41:20 -0500

    feat: functions

---
+++ basic/functions.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Functions {
+    /**
+        function (<parameter types>) {internal|external} [pure|view|payable] [returns (<return types>)]
+
+        - pure functions can be converted to view and non-payable functions
+        - view functions can be converted to non-payable functions
+        - payable functions can be converted to non-payable functions
+    */
+
+    /**
+        Pure
+    */
+    function sumPureFunction(uint256 number1, uint256 number2)
+        public
+        pure
+        returns (uint256)
+    {
+        uint256 sum = number1 + number2;
+        return sum;
+    }
+
+    /**
+        View
+    */
+
+    // declare a state variable
+    uint256 uintStateVar = 10;
+
+    function sumViewFunction(uint256 number1, uint256 number2)
+        public
+        view
+        returns (uint256 sum)
+    {
+        sum = uintStateVar + number1 + number2;
+    }
+
+    /**
+        Payable
+    */
+    function transfer(address payable _to, uint256 _amount) public payable {
+        // logic
+    }
+
+    /**
+        Default functions
+
+                            send Ether
+                                |
+                            msg.data is empty?
+                                /       \
+                            yes        no
+                            /           \
+                    receive() exists?     fallback()
+                            /     \
+                        yes        no
+                        /           \
+                    receive()      fallback()
+    */
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
