3f1877668e724f7cacb683a40a20813f23d5c6a3
Martín Alexis Samán Arata <martinsam1610@gmail.com>
2022-09-13 06:16:46 -0500

    feat: constructor declaration

--- basic/smart-contract-structure.sol
+++ basic/smart-contract-structure.sol
@@ -4,6 +4,11 @@ pragma solidity >=0.7.0 <0.9.0;     // Compiler version
 pragma abicoder v2;                 // Abi coder version (optional)
 
 contract ContractDeclaration {
+
+    // Constructor declaration
+    constructor () {
+        // ...
+    }
     
     // code
 
