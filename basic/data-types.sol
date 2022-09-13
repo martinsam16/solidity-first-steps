3139b066f38c5f8b79cfec59a825412f69bef408
Martín Alexis Samán Arata <martinsam1610@gmail.com>
2022-09-13 06:14:26 -0500

    feat: data types

---
+++ basic/data-types.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract DataTypes {
+
+    /**
+        Booleans
+    */
+    bool boolVar = true;
+
+    /**
+        String
+    */
+    string stringVar = "Hello world :D";
+    string stringUnicodeVar = unicode"Hello world 😃";
+
+    /**
+        Address
+    */
+    address addressVar = 0x4901C620133DF0b2a6e41EDD718430B9D31beD6E;
+    address payable addressVarPayable = payable(0x4901C620133DF0b2a6e41EDD718430B9D31beD6E);
+
+    /**
+        Bytes
+    */
+    bytes byteVar;
+    bytes1 byteVar1;
+    bytes32 byteVar32;
+
+    /**
+        Unsigned Int
+    */
+    uint256 unsignedIntVar;
+    uint8 unsignedIntVar8;
+    uint256 unsignedIntVar256;
+
+    /**
+        Signed Int
+    */
+    int256 signedIntVar;
+    int8 signedIntVar8;
+    int256 signedIntVar256;
+
+    /**
+        Enums
+    */
+    enum EnumType {
+        ON,
+        OFF
+    }
+
+    /**
+        User Defined Value
+    */
+    type customType is uint256;
+    type customTypeBool is bool;
+
+    /**
+        Arrays
+    */
+    uint[] uintArray = new uint[](7);
+    int[] intArray = new int[](9);
+    int[][] intBidimentionalArray = new int[][](3); // not recommendable
+
+    /**
+        Mappings (key, value)
+    */
+    mapping (uint => string) mappingVar;
+    mapping (address => int) mappingVar2;
+
+    /**
+        Structs
+    */
+    struct StructType {
+        uint[] var1;
+        uint var2;
+        string var3;
+        // ...
+    }
+    StructType structType;
+}
