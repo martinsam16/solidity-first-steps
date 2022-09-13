1bf59e88b271f2fe1c39915f86c299129fdc0122
Martín Alexis Samán Arata <martinsam1610@gmail.com>
2022-09-13 05:31:19 -0500

    feat: basic structure of smart contract

---
+++ basic/smart-contract-structure.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;     // Compiler version
+pragma abicoder v2;                 // Abi coder version (optional)
+
+contract ContractDeclaration {
+    
+    // code
+
+}
